a8e2d32b3857abe545be857e21a33e40f4781694
Alessandro Ciccarone <ale70029@gmail.com>
2024-09-15 23:08:45 +0200

    updated translation

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -7,7 +7,7 @@ import styles from "./Experience.module.css"
 export const Experience = () => {
   return (
     <section id='experience' className={styles.container}>
-        <h2 className={styles.title}>Experience</h2>
+        <h2 className={styles.title}>Esperienza</h2>
         <div className={styles.content}>
             <div className={styles.skills}>
                 {skills.map((skill,id) =>{
