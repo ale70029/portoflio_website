c084df01530099d2b6681f2f57e49e57d7d39eca
Alessandro Ciccarone <ale70029@gmail.com>
2024-09-16 20:41:31 +0200

    removed portfolio useless link

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,63 +6,63 @@ import LangContext from "../../LanguageContext";
 
 export const Navbar = () =>{
     const [menuOpen,setMenuOpen] = useState(false);
-    const [flagOpen,setflagOpen] = useState(false);
+    const [flagOpen,setFlagOpen] = useState(false);
     const { lang, setLang } = useContext(LangContext);
-    return (<nav className={styles.navbar}>
-    <a href="/" className={styles.title}>{text[lang].navHome}</a>
-    
-    <div className={styles.menu1}>
-        <img 
-            className={styles.menuBtn}
-            src={getImageUrl(`nav/it.png`)}
-            alt="menu-button"
-        />
-        <ul
-           className={`${styles.menuItems} ${flagOpen && styles.menuOpen}`}
-           onClick={() => setflagOpen(false)}
-        >
-            <li>
-                <img src={getImageUrl("nav/it.png")} alt="it" onClick={() => setLang("it")}/>
-            </li>
-            <li>
-                <img src={getImageUrl("nav/en.png")} alt="en" onClick={() => setLang("en")}/>
-            </li>
-            <li>
-                <img src={getImageUrl("nav/es.png")} alt="es" onClick={() => setLang("es")}/>
-            </li>
-        </ul>
-    </div>
-    
-    <div className={styles.menu2}>
-        <img 
-            className={styles.menuBtn}
-            src={
-                menuOpen
-                ? getImageUrl("nav/closeIcon.png")
-                : getImageUrl("nav/menuIcon.png")
-            }    
-            alt="menu-button"
-            onClick={() => setMenuOpen(!menuOpen)}
-        />
-        <ul
-            className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
-            onClick={() => setMenuOpen(false)}
-        >
-            <li>
-                <a href="#about">{text[lang].navAbout}</a>
-            </li>
-            <li>
-                <a href="#experience">{text[lang].navExperience}</a>
-            </li>
-            <li>
-                <a href="#projects">{text[lang].navProjects}</a>
-            </li>
-            <li>
-                <a href="#contact">{text[lang].navContact}</a>
-            </li>
-        </ul>
-    </div>
-
+    return (
+        
+    <nav className={styles.navbar}>
+        <div className={styles.langMenu}>
+            <img 
+                className={styles.langBtn}
+                src={getImageUrl(`nav/${lang}.png`)}
+                alt="menu-button"
+                onClick={() => setFlagOpen(!flagOpen)}
+            />
+            <ul
+            className={`${styles.langItems} ${flagOpen && styles.langOpen}`}
+            onClick={() => setFlagOpen(false)}
+            >
+                <li>
+                    <img src={getImageUrl("nav/it.png")} alt="it" onClick={() => setLang("it")}/>
+                </li>
+                <li>
+                    <img src={getImageUrl("nav/en.png")} alt="en" onClick={() => setLang("en")}/>
+                </li>
+                <li>
+                    <img src={getImageUrl("nav/es.png")} alt="es" onClick={() => setLang("es")}/>
+                </li>
+            </ul>
+        </div>
+        
+        <div className={styles.sectionsMenu}>
+            <img 
+                className={styles.menuBtn}
+                src={
+                    menuOpen
+                    ? getImageUrl("nav/closeIcon.png")
+                    : getImageUrl("nav/menuIcon.png")
+                }    
+                alt="menu-button"
+                onClick={() => setMenuOpen(!menuOpen)}
+            />
+            <ul
+                className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+                onClick={() => setMenuOpen(false)}
+            >
+                <li>
+                    <a href="#about">{text[lang].navAbout}</a>
+                </li>
+                <li>
+                    <a href="#experience">{text[lang].navExperience}</a>
+                </li>
+                <li>
+                    <a href="#projects">{text[lang].navProjects}</a>
+                </li>
+                <li>
+                    <a href="#contact">{text[lang].navContact}</a>
+                </li>
+            </ul>
+        </div>
     </nav>
     );
 };
