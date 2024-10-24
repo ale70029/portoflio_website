f4e9dfe0204209c0208945c859fb27d3aa6e99da
Alessandro Ciccarone <ale70029@gmail.com>
2024-10-24 02:32:08 +0200

    about 3.0

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,13 +2,28 @@ import React, { useContext } from 'react'
 import {getImageUrl} from "../../utils"
 import styles from "./About.module.css"
 import about from "../../data/about.json"
-import { text } from '../../data/translation'
+// import { text } from '../../data/translation'
 import LangContext from "../../LanguageContext";
 
 export const About = () => {
-    const { lang, setLang } = useContext(LangContext);
+  const { lang, setLang } = useContext(LangContext);
+
   return (
-    <div className={styles.container} id='about'>
+    <section className={styles.about} id='about'>
+        <ul className={styles.roles}>
+                {about.map((about,id) =>{
+                    return <li key={id} className={styles.role}>
+                                <img src={getImageUrl(about.icon)} alt="icon" />
+                                <h1>{about.title[lang]}</h1>
+                                <p>{about.description[lang]}</p>
+                            </li>
+                })}   
+        </ul>
+    </section>
+    
+  )
+}
+{/* <div className={styles.container} id='about'>
         <h2 className={styles.title}>{text[lang].navAbout}</h2>
         <div className={styles.content}>
             <ul className={styles.aboutItems}>
@@ -26,6 +41,4 @@ export const About = () => {
                     })}   
             </ul>
         </div>
-    </div>
-  )
-}
+    </div> */}
